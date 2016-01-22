67cbb43c3a968595cd6bd47cdd6b2c4e3735fda7
Felix Yuan <felix.yuan@emc.com>
2016-01-22 20:29:21 +0800

    Improve log output
    - default turn off colorful output, user can turn on the colorful output by configuration: --color or --logColorEnable
    - print full log level name
    - print caller uses the same color of corresponding log level
    - change the color for 'debug' level to cyan
    - improve LogEvent unittest coverage

--- index.js
+++ index.js
@@ -9,54 +9,78 @@ require('./lib/extensions');
 module.exports = function (di, directory) {
     var helper = require('./lib/di')(di, directory || __dirname);
 
+    var injectables = _.flattenDeep(
+        [
+            // NPM Packages
+            helper.simpleWrapper(_, '_'),
+            helper.requireWrapper('bluebird', 'Promise'),
+            helper.requireWrapper('rx', 'Rx'),
+            helper.requireWrapper('nconf'),
+            helper.requireWrapper('waterline', 'Waterline'),
+            helper.requireWrapper('waterline-criteria', 'WaterlineCriteria'),
+            helper.requireWrapper('sails-mongo', 'MongoAdapter'),
+            helper.requireWrapper('amqp', 'amqp'),
+            helper.requireWrapper('domain', 'domain'),
+            helper.requireWrapper('node-uuid', 'uuid'),
+            helper.requireWrapper('stack-trace', 'stack-trace'),
+            helper.requireWrapper('colors/safe', 'colors'),
+            helper.requireWrapper('prettyjson', 'prettyjson'),
+            helper.requireWrapper('lru-cache', 'lru-cache'),
+            helper.requireWrapper('node-statsd', 'node-statsd'),
+            helper.requireWrapper('validate.js', 'validate'),
+            helper.requireWrapper('validator', 'validator'),
+            helper.requireWrapper('assert-plus', 'assert-plus'),
+            helper.requireWrapper('ejs', 'ejs'),
+            helper.requireWrapper('hogan.js', 'Hogan'),
+            helper.requireWrapper('fs', 'fs'),
+            helper.requireWrapper('path', 'path'),
+            helper.requireWrapper('child_process', 'child_process'),
+            helper.requireWrapper('anchor', 'anchor'),
+            helper.requireWrapper('jsonschema', 'jsonschema'),
+            helper.simpleWrapper(console, 'console'),
+            helper.simpleWrapper(require('eventemitter2').EventEmitter2, 'EventEmitter'),
+            helper.requireWrapper('shortid', 'shortid'),
+            helper.requireWrapper('crypto', 'crypto'),
+            helper.requireWrapper('apache-crypt', 'apache-crypt'),
+            helper.requireWrapper('util', 'util'),
+            helper.requireWrapper('pluralize', 'pluralize'),
+            helper.requireWrapper('blocked', 'blocked'),
+            helper.requireWrapper('always-tail', 'Tail'),
+            helper.requireWrapper('flat', 'flat'),
+
+            // Glob Requirables
+            helper.requireGlob(__dirname + '/lib/common/*.js'),
+            helper.requireGlob(__dirname + '/lib/models/*.js'),
+            helper.requireGlob(__dirname + '/lib/protocol/*.js'),
+            helper.requireGlob(__dirname + '/lib/serializables/*.js'),
+            helper.requireGlob(__dirname + '/lib/services/*.js')
+        ]
+    );
+
+    var injector = new di.Injector(injectables);
+    handleConfig(injector);
+
     return {
         helper: helper,
-        injectables: _.flattenDeep(
-            [
-                // NPM Packages
-                helper.simpleWrapper(_, '_'),
-                helper.requireWrapper('bluebird', 'Promise'),
-                helper.requireWrapper('rx', 'Rx'),
-                helper.requireWrapper('nconf'),
-                helper.requireWrapper('waterline', 'Waterline'),
-                helper.requireWrapper('waterline-criteria', 'WaterlineCriteria'),
-                helper.requireWrapper('sails-mongo', 'MongoAdapter'),
-                helper.requireWrapper('amqp', 'amqp'),
-                helper.requireWrapper('domain', 'domain'),
-                helper.requireWrapper('node-uuid', 'uuid'),
-                helper.requireWrapper('stack-trace', 'stack-trace'),
-                helper.requireWrapper('colors/safe', 'colors'),
-                helper.requireWrapper('prettyjson', 'prettyjson'),
-                helper.requireWrapper('lru-cache', 'lru-cache'),
-                helper.requireWrapper('node-statsd', 'node-statsd'),
-                helper.requireWrapper('validate.js', 'validate'),
-                helper.requireWrapper('validator', 'validator'),
-                helper.requireWrapper('assert-plus', 'assert-plus'),
-                helper.requireWrapper('ejs', 'ejs'),
-                helper.requireWrapper('hogan.js', 'Hogan'),
-                helper.requireWrapper('fs', 'fs'),
-                helper.requireWrapper('path', 'path'),
-                helper.requireWrapper('child_process', 'child_process'),
-                helper.requireWrapper('anchor', 'anchor'),
-                helper.requireWrapper('jsonschema', 'jsonschema'),
-                helper.simpleWrapper(console, 'console'),
-                helper.simpleWrapper(require('eventemitter2').EventEmitter2, 'EventEmitter'),
-                helper.requireWrapper('shortid', 'shortid'),
-                helper.requireWrapper('crypto', 'crypto'),
-                helper.requireWrapper('apache-crypt', 'apache-crypt'),
-                helper.requireWrapper('util', 'util'),
-                helper.requireWrapper('pluralize', 'pluralize'),
-                helper.requireWrapper('blocked', 'blocked'),
-                helper.requireWrapper('always-tail', 'Tail'),
-                helper.requireWrapper('flat', 'flat'),
-
-                // Glob Requirables
-                helper.requireGlob(__dirname + '/lib/common/*.js'),
-                helper.requireGlob(__dirname + '/lib/models/*.js'),
-                helper.requireGlob(__dirname + '/lib/protocol/*.js'),
-                helper.requireGlob(__dirname + '/lib/serializables/*.js'),
-                helper.requireGlob(__dirname + '/lib/services/*.js')
-            ]
-        )
+        injectables: injectables
     };
 };
+
+/**
+ * Handle the configuration value
+ *
+ * TODO: This is not good place to put such configuration handle code especially when we have
+ * lot of configuration need to be handled in future. So we need to move this to a better place.
+ *
+ * @param {Object} injector - DI injector instance used to retrieve dependancy modules
+ */
+function handleConfig(injector) {
+    var configuration = injector.get('Services.Configuration');
+
+    // Handle Log Color
+    var LogEvent = injector.get('LogEvent');
+    var colorEnable = configuration.get('color', false, true) ||
+                      configuration.get('logColorEnable', false, true);
+    LogEvent.setColorEnable(colorEnable);
+}
+
